05ae8a3b2d242ddbddcec4b3f45ceac20028577a
mohamed Ibrahim <muhmds@hotmail.com>
2020-03-20 22:05:42 -0400

    final push

--- public/assets/js/burgers_app.js
+++ public/assets/js/burgers_app.js
@@ -30,7 +30,7 @@ $(function () {
       };
 
       // Send the POST request.
-      $.ajax("/api/burgers", {
+      $.ajax("/api/burgers/", {
         type: "POST",
         data: newBurger
       }).then(function () {
